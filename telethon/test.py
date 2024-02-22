b023b5d37a3dd6e49d7b5e3c77ffbc3729312c74
Vladimir <shinkarenkovova@gmail.com>
2024-02-22 15:05:38 +0300

    del

--- telethon/test.py
+++
@@ -1,7 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-with TelegramClient(StringSession(), 26527851, '3487105350b815cd247ba22f6976d764') as client:
-    print(client.session.save())
-
-    
